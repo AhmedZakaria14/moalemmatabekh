75da0139d7e1d28cfb0804bd8faa3fd27d47746a
AhmedZakaria14 <azeco77@gmail.com>
2026-08-29 22:59:28 +0300

    chore: remove obsolete Cloudinary image host

--- next.config.ts
+++ next.config.ts
@@ -23,12 +23,6 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        port: '',
-        pathname: '/**',
-      },
     ],
   },
   output: 'standalone',
